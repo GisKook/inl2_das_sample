50dcfe0857d9e809f68d9a51bfc1e83c253fb2fc
zhangkai <zhangkai.gis@163.com>
2017-06-19 09:19:44 +0800

    new topic

--- mqtt_srv/mqtt_srv_socket_send.go
+++ mqtt_srv/mqtt_srv_socket_send.go
@@ -15,8 +15,8 @@ var f MQTT.MessageHandler = func(client MQTT.Client, msg MQTT.Message) {
 func (mqtt_socket *Mqtt_srv_socket) send(p *base.BluetoothRing) {
 	payload, _ := json.Marshal(p)
 	log.Println(payload)
-	log.Println(mqtt_socket.Options.TopicPub)
-	if token := mqtt_socket.Mqtt_socket.Publish(mqtt_socket.Options.TopicPub, 0, false, payload); token.Wait() && token.Error() != nil {
+	//log.Println(mqtt_socket.Options.TopicPub)
+	if token := mqtt_socket.Mqtt_socket.Publish(p.RingMac, 0, false, payload); token.Wait() && token.Error() != nil {
 		log.Println(token.Error())
 	}
 
